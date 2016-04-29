4fbc3d9cc27b6023da625825b1e8f538a5f37509
Gavi <grawson2@jhu.edu>
2016-04-29 15:46:54 -0400

    layout for login

--- Gopher/app/src/main/java/com/example/gavi/gopher/LoginActivity.java
+++ Gopher/app/src/main/java/com/example/gavi/gopher/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.gavi.gopher;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Activity activity = this;
+        ((Button) findViewById(R.id.loginButton)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserSwitcherActivity.chooseUI(getApplicationContext(), (activity));
+            }
+        });
     }
 }
