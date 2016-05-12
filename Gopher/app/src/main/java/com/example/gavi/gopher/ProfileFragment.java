5f6675eb1daf529a744e9074ce83ded61624c200
wyao7 <wyao7@jhu.edu>
2016-05-12 04:56:53 +0200

    Profile now works with a static id "Vonnie", needs fixing to make it work with actual ID

--- Gopher/app/src/main/java/com/example/gavi/gopher/ProfileFragment.java
+++ Gopher/app/src/main/java/com/example/gavi/gopher/ProfileFragment.java
@@ -1,17 +1,31 @@
 package com.example.gavi.gopher;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    private String userid;
+    private TextView gopherView;
+    private TextView soldView;
+    private TextView boughtView;
+
 
     View view;
 
@@ -28,7 +42,57 @@ public class ProfileFragment extends Fragment {
                 R.id.profile_header_container, new ProfileHeaderFragment()
         ).commit();
 
+        gopherView = (TextView) view.findViewById(R.id.gopher_points_field);
+        soldView = (TextView) view.findViewById(R.id.meals_sold_field);
+        boughtView = (TextView) view.findViewById(R.id.meals_bought_field);
+
+        setData();
+
         return view;
+    }
+
+
+    //setup data on page
+    private void setData() {
+
+        //get userID
+        SharedPreferences myPrefs =  PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        userid = myPrefs.getString(Constants.USER_ID, "");
+        Firebase userRef = Modules.connectDB(getActivity(), "/meals_bought/" + "Vonnie");
+
+        //get user ID -> meal ID to get meal
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int GopherPoint;
+                int mealsSold;
+                int mealsBought;
+                String mealsSoldString = dataSnapshot.child("mealsSold").getValue().toString();
+                String mealsBoughtString = dataSnapshot.child("mealsBought").getValue().toString();
+                System.out.println(mealsSoldString + " and " + mealsBoughtString);
+                if (mealsSoldString.equals("")) {
+                    mealsSold = 0;
+                } else {
+                    mealsSold = Integer.parseInt(mealsSoldString);
+                }
+                if (mealsBoughtString.equals("")) {
+                    mealsBought = 0;
+                } else {
+                    mealsBought = Integer.parseInt(mealsBoughtString);
+                }
+
+                GopherPoint = (mealsBought * 1) + (mealsSold * 5);
+                gopherView.setText("" + GopherPoint);
+                soldView.setText("" + mealsSold);
+                boughtView.setText("" + mealsBought);
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
     }
 
 }
